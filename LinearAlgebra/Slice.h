974299ca70f90fe25f77aabadec2e780896c16b2
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-25 13:02:26 +0800

    fix: template args should auto fill for slice

--- LinearAlgebra/Slice.h
+++ LinearAlgebra/Slice.h
@@ -18,7 +18,7 @@ namespace MathLib {
 				}
 
 				T At(const unsigned& r, const unsigned& c) const {
-					return  expr.At(r, c);
+					return  expr.At(r + row, c + column);
 				}
 
 				constexpr unsigned Rows() const {
@@ -36,7 +36,7 @@ namespace MathLib {
 
 					for (unsigned i = 0; i < Rows(); ++i) {
 						for (unsigned j = 0; j < Columns(); ++j) {
-							result[i][j] = expr.At(j, i);
+							result[i][j] = expr.At(i + row, j + column);
 						}
 					}
 
@@ -45,10 +45,12 @@ namespace MathLib {
 			};
 		}
 
-		template<typename T, typename Expr, const unsigned _Rows, const unsigned _Columns>
-		detail::MatrixSlice<T, Expr, _Rows, _Columns> Slice(const Expr& expr, const unsigned& row, const unsigned& column) {
-			return detail::MatrixSlice<T, Expr, _Rows, _Columns>(expr, row, column);
+		template< const unsigned _Row, const unsigned _Column, typename T, typename Expr>
+		detail::MatrixSlice<T, Expr, _Row, _Column> Slice(const MatrixExpression<T, Expr>& expr, const unsigned row, const unsigned column) {
+			return detail::MatrixSlice<T, Expr, _Row, _Column>(
+				*static_cast<const Expr*>(&expr), 
+				row, column
+			);
 		}
-
 	}
 }
