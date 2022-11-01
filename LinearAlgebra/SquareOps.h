b61f364af4f81e3f5bfd50432fd643d3c940c55b
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-11-01 12:50:59 +0800

    chore: refactor

--- LinearAlgebra/SquareOps.h
+++ LinearAlgebra/SquareOps.h
@@ -10,10 +10,10 @@ namespace MathLib {
 		using SquareMatrix = Matrix<T, N, N>;
 
 		template<typename T, const unsigned N, typename Derived>
-		using SquareMatrixExpression = MatrixBase<T, N, N, Derived>;
+		using SquareMatrixBase = MatrixBase<T, N, N, Derived>;
 
 		template<typename T, const unsigned N, typename Exp>
-		bool IsUpper(const SquareMatrixExpression<T, N, Exp>& mat) {
+		bool IsUpper(const SquareMatrixBase<T, N, Exp>& mat) {
 			for (unsigned i = 1; i < N; ++i) {
 				for (unsigned j = 0; j < i - 1; ++j) {
 					if (mat.At(i, j) != T()) {
@@ -25,7 +25,7 @@ namespace MathLib {
 		}
 
 		template<typename T, const unsigned N, typename Exp>
-		bool IsLower(const SquareMatrixExpression<T, N, Exp>& mat) {
+		bool IsLower(const SquareMatrixBase<T, N, Exp>& mat) {
 			for (unsigned i = 0; i < N; ++i) {
 				for (unsigned j = i + 1; j < N; ++j) {
 					if (mat.At(i, j) != T()) {
@@ -37,7 +37,7 @@ namespace MathLib {
 		}
 
 		template<typename T, const unsigned N, typename Exp>
-		bool IsDiagonal(const SquareMatrixExpression<T, N, Exp>& mat) {
+		bool IsDiagonal(const SquareMatrixBase<T, N, Exp>& mat) {
 			for (unsigned i = 0; i < N; ++i) {
 				for (unsigned j = 0; j < N; ++j) {
 					if (i != j && mat.At(i, j) != T()) {
@@ -50,7 +50,7 @@ namespace MathLib {
 		}
 
 		template<typename T, const unsigned N, typename Exp>
-		bool IsSymmetric(const SquareMatrixExpression<T, N, Exp>& mat) {
+		bool IsSymmetric(const SquareMatrixBase<T, N, Exp>& mat) {
 			for (unsigned i = 0; i < N; ++i) {
 				for (unsigned j = i + 1; j < N; ++j) {
 					if (mat.At(i, j) != mat.At(j, i)) {
