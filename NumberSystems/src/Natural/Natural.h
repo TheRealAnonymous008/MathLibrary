487112485d45d47d6c74e5a76e006b987203dd4d
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-11-30 14:04:21 +0800

    add: basic natural numbers

--- NumberSystems/src/Natural/Natural.h
+++ NumberSystems/src/Natural/Natural.h
@@ -1,10 +1,23 @@
 #include "../../headers/Fwd.h"
+#include "NaturalBase.h"
+
+#include "../../Exceptions.h"
 
 namespace MathLib {
 	namespace NumberSystems {
 
-		class Natural {
+		class Natural : NaturalBase{
+		private:
+			string_type value;
 
+		public :
+			Natural(string_type value = "0") : value(value) {
+
+			}
+
+			string_type Val() const {
+				return this->value;
+			}
 		};
 
 	}
--- _Tests/_NumberSystems/Natural.cpp
+++ _Tests/_NumberSystems/Natural.cpp
@@ -3,6 +3,7 @@
 using namespace MathLib::NumberSystems;
 
 TEST(Natural, Basic) {
-	Natural x = Natural();
-
+	Natural x = Natural("1234567");
+	
+	ASSERT_EQ(x.Val(), "1234567");
 }
