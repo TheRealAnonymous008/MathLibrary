bcb0059c1a1ca331d5f062a5ec0fd3ce9e50b268
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-12-01 08:04:01 +0800

    proto: add natural addition

--- NumberSystems/src/Natural/NaturalAddition.h
+++ NumberSystems/src/Natural/NaturalAddition.h
@@ -5,8 +5,29 @@
 namespace MathLib {
 	namespace NumberSystems {
 
+		namespace detail {
 
+			template<typename LHS, typename RHS>
+			class NaturalAddition : NaturalBase<NaturalAddition> {
+			private:
+				const LHS& lhs;
+				const RHS& rhs;
 
+			public: 
+				NaturalAddition(const LHS& lhs, const RHS& rhs) : lhs(lhs), rhs(rhs) {
+					
+				}
+
+			};
+		}
+
+		template<typename LHS, typename RHS>
+		detail::NaturalAddition<LHS, RHS> operator+(
+			const NaturalBase<LHS>& lhs,
+			const NaturalBase<RHS>& rhs)
+		{
+			return detail::NaturalAddition(*static_cast<const LHS*>(&lhs), *static_cast<const RHS*>(&rhs));
+		}
 
 	}
 }
