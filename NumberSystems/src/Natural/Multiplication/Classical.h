f301b0c0ccf6ddf16ea106038ebb762777248604
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-12-06 12:02:07 +0800

    add: additional stress test for multiplication

--- NumberSystems/src/Natural/Multiplication/Classical.h
+++ NumberSystems/src/Natural/Multiplication/Classical.h
@@ -7,8 +7,6 @@ namespace MathLib {
 	namespace NumberSystems {
 		namespace implementation {
 
-			
-
 			template<typename LHS, typename RHS>
 			Natural ClassicalMultiplication(const NaturalBase<LHS>& lhs, const NaturalBase<RHS>& rhs) {
 				Natural result;
--- NumberSystems/src/Natural/Multiplication/Karatsuba.h
+++ NumberSystems/src/Natural/Multiplication/Karatsuba.h
@@ -6,7 +6,12 @@ namespace MathLib {
 	namespace NumberSystems {
 		namespace implementation {
 
+			template<typename LHS, typename RHS>
+			Natural KaratsubaMultiplication(const NaturalBase<LHS>& lhs, const NaturalBase<RHS>& rhs) {
+				Natural result;
 
+				return result;
+			}
 
 		}
 	}
