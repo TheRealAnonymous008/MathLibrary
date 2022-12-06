59c289966d274ad1c2375b49a4e2da39ae7b0263
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-12-06 14:28:31 +0800

    proto: natural division

--- NumberSystems/headers/Natural.h
+++ NumberSystems/headers/Natural.h
@@ -2,6 +2,7 @@
 
 #include "../src/Natural/Natural.h"
 #include "../src/Natural/NaturalAddition.h"
+#include "../src/Natural/NaturalDivision.h"
 #include "../src/Natural/NaturalMultiplication.h"
 #include "../src/Natural/NaturalOrder.h"
 #include "../src/Natural/NaturalSubtraction.h"
---
+++ NumberSystems/src/Natural/NaturalDivision.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "../../Exceptions.h"
+
+#include "Natural.h"
+
+namespace MathLib {
+	namespace NumberSystems {
+
+		namespace detail {
+
+			template<typename LHS, typename RHS>
+			class NaturalDivision : public NaturalBase<NaturalDivision<LHS, RHS>> {
+			private:
+				const LHS& lhs;
+				const RHS& rhs;
+
+				Natural result;
+
+				void Calculate() {
+
+				}
+
+			public:
+				NaturalDivision(const LHS& lhs, const RHS& rhs) : lhs(lhs), rhs(rhs) {
+					Calculate();
+				}
+
+				const vector_type Digits() const {
+					return result.Digits();
+				}
+
+				auto Evaluate() const {
+					return result;
+				}
+
+			};
+		}
+
+		template<typename LHS, typename RHS>
+		detail::NaturalDivision<LHS, RHS> operator/(const NaturalBase<LHS>& lhs, const NaturalBase<RHS>& rhs) {
+			return detail::NaturalDivision(*static_cast<const LHS*>(&lhs), *static_cast<const RHS*>(&rhs));
+		}
+
+	}
+}
