d8d47df7e8f27a877eddd6e370169fcc16ab8e40
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-12-16 21:38:09 +0800

    add: support for critical sections in openmp

--- Common/Options.h
+++ Common/Options.h
@@ -2,8 +2,11 @@
 
 #ifdef _OPENMP_LLVM_RUNTIME 
 #define OPENMP_PARALLELIZE _Pragma("omp parallel for")
+#define OPENMP_CRITICAL_SECTION _Pragma("omp critical")
 #elif _OPENMP > 200805
 #define OPENMP_PARALLELIZE _Pragma("omp parallel for")
+#define OPENMP_CRITICAL_SECTION _Pragma("omp critical")
 #else
 #define OPENMP_PARALLELIZE 
+#define OPENMP_CRITICAL_SECTION 
 #endif
