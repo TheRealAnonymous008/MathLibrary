061baae11f79d6a99c7109eb6c4f578d55a558b6
JoaquinDelaRosa <80933658+JoaquinDelaRosa@users.noreply.github.com>
2022-12-28 08:11:07 +0800

    proto: update functor templates

--- NumberSystems/headers/Fwd.h
+++ NumberSystems/headers/Fwd.h
@@ -31,5 +31,38 @@ namespace MathLib {
 		class IntegerBase;
 
 		class Integer;
+<<<<<<< Updated upstream
+=======
+		
+		namespace detail {
+			template<typename F, typename LHS, typename RHS> class Addition {};
+			template<typename F, typename LHS, typename RHS> class Subtraction {};
+			template<typename F, typename LHS, typename RHS> class Multiplication {};
+			template<typename F, typename LHS, typename RHS> class Division {};
+			template<typename F, typename LHS, typename RHS> class Modulo {};
+
+			template<typename F, typename Expr> class Negate {};
+			template<typename F, typename Expr> class Not {};
+
+			template<typename F, typename LHS, typename RHS> class And {};
+			template<typename F, typename LHS, typename RHS> class Or {};
+			template<typename F, typename LHS, typename RHS> class Xor {};
+			template<typename F, typename LHS, typename RHS> class LeftShift {};
+			template<typename F, typename LHS, typename RHS> class RightShift {};
+
+			template<typename F, typename Expr> class Sin {};
+			template<typename F, typename Expr> class Cos {};
+			template<typename F, typename Expr> class Tan {};
+			template<typename F, typename Expr> class Csc {};
+			template<typename F, typename Expr> class Sec {};
+			template<typename F, typename Expr> class Cot {};
+
+			template<typename F, typename Expr> class Sqrt {};
+			template<typename F, typename Expr> class Square {};
+			template<typename F, typename Expr> class Factorial {};
+
+			template<typename F, typename Base, typename Power> class Pow {};
+		}
+>>>>>>> Stashed changes
 	}
 }
